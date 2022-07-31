0a78d3e437c531548ebb78b8922b2bd8af9580a6
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-07-31 16:02:57 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React, { Component } from 'react'
-import './App.css'
-import Form from './components/Form'
+import "./App.css";
+// import Sidebar from './Sidebar';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export class App extends Component {
-  getHero = (e) => {
-    const heroName = e.target.elements.heroName.value;
-    e.preventDefault();
-    console.log(heroName);
-  }
-  render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1>Hero Search</h1>
-        </header>
-        <Form getHero={this.getHero} />
-      </div>
-    )
-  }
+function App() {
+  const [heroes, setHeroes] = useState([]);
+
+  const hero = () => {
+    axios
+      .get("https://akabab.github.io/superhero-api/api/all.json")
+      .then((response) => {
+        console.log(response);
+        setHeroes(response.data);
+      });
+  };
+
+  useEffect(() => {
+    hero();
+  }, []);
+  return (
+    <div className="">
+      {/* <ul>
+        {heroes.map(user => (
+          <li key={user.id}>
+            <img src={user.images.xs} alt="" />
+            <h2> {user.name} </h2>
+            
+          </li>
+                 ))
+        }
+      </ul> */}
+      <form onSubmit={hero}>
+        <input type="text" name="heroName" />
+        <button>Search</button>
+      </form>
+      {heroes.map((user) => (
+        <li key={user.id}>
+          <img src={user.images.xs} alt="" />
+          <h2> {user.name} </h2>
+        </li>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
